aa373b6bb18500b8818b2c9fcff061cfe2150469
anuskaleem <anuskaleem@gmail.com>
2020-02-28 16:01:24 +0500

    Update PickerItem.js

--- src/basic/PickerItem.js
+++ src/basic/PickerItem.js
@@ -1,12 +1,10 @@
 import React, { Component } from 'react';
 import { Picker } from 'react-native';
 import { connectStyle } from 'native-base-shoutem-theme';
-import Reactotron from 'reactotron-react-native'
 
 import mapPropsToStyleNames from '../utils/mapPropsToStyleNames';
 
 class Item extends Component {
-  Reactotron.logImportant("logging from picker item")
   console.log("logging from picker item")
   render() {
     return <Picker.Item ref={c => (this._root = c)} {...this.props} />;
